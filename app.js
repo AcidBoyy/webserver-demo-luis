54b756a43af70a42b0f9c4ab0b66d0f8dc7d727c
AcidBoyy <76119156+AcidBoyy@users.noreply.github.com>
2023-11-02 14:32:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Das Fach vo de IBM isch echt cool!')
 })
 
 app.get('/port', (req, res) => {
