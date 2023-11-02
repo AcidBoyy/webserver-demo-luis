e2c9213f3a164926497ff0a08450e3c670609b0d
Luis Mende <76119156+AcidBoyy@users.noreply.github.com>
2023-11-02 15:08:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,75 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('De Nathan wird bide Wohnigsbesichtigung medem Vermierter flirte!')
-})
+  res.send(`
+    <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f0f0f0;
+        }
+        .container {
+          text-align: center;
+          margin-top: 20px;
+        }
+        .title {
+          font-size: 24px;
+          color: #3366cc;
+        }
+        .message {
+          font-size: 18px;
+          color: #ff6600;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1 class="title">Herzlich Willkommen zur Wohnungsbesichtigung!</h1>
+        <p class="message">De Nathan wird bei der Wohnungsbesichtigung den Vermieter beeindrucken!</p>
+        <p class="message">Vielen Dank, dass Sie unsere Website besuchen.</p>
+      </div>
+    </body>
+    </html>
+  `);
+});
 
 app.get('/port', (req, res) => {
-  res.send(`Currently used port is ${port}`)
-})
+  res.send(`
+    <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f0f0f0;
+        }
+        .container {
+          text-align: center;
+          margin-top: 20px;
+        }
+        .title {
+          font-size: 24px;
+          color: #3366cc;
+        }
+        .message {
+          font-size: 18px;
+          color: #ff6600;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1 class="title">Aktuell verwendeter Port</h1>
+        <p class="message">Der verwendete Port ist ${port}</p>
+      </div>
+    </body>
+    </html>
+  `);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Die Beispielanwendung lauscht auf Port ${port}`);
+});
+
